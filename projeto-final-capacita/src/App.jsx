c27006401e4a960d1912129d1a97ee710bf059f3
Rhaniacell <rhaniatvares57@gmail.com>
2025-01-28 20:40:41 -0300

    Criada Rota para Feedback

--- projeto-final-capacita/src/App.jsx
+++ projeto-final-capacita/src/App.jsx
@@ -1,44 +1,27 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importações para rotas
-import Home from './components/Home';
-import CadastroProduto from './components/CadastroProduto'; 
-import { Modal, Box, Button } from '@mui/material';
-import FeedbackUsuario from './components/FeedbackUsuario';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; // Removi o useNavigate
+import Home from "./components/Home";
+import CadastroProduto from "./components/CadastroProduto";
+import FeedbackUsuario from "./components/FeedbackUsuario";
+import { Button } from "@mui/material";
 
 function App() {
-  const [modalFeedback, setModalFeedback] = useState(false);
-
   return (
-    <Router> {}
+    <Router>
       <Routes>
-        <Route path="/" element={<Home />} /> {}
-        <Route path="/cadastrar-produto" element={<CadastroProduto />} /> {}
+        <Route path="/" element={<Home />} />
+        <Route path="/cadastrar-produto" element={<CadastroProduto />} />
+        <Route path="/feedback" element={<FeedbackUsuario />} />
       </Routes>
       <Button
+        component={Link}
+        to="/feedback"
         variant="contained"
         color="secondary"
-        onClick={() => setModalFeedback(true)}
-        sx={{ position: 'fixed', bottom: '20px', right: '20px' }}
+        sx={{ position: "fixed", bottom: "20px", right: "20px" }}
       >
         Feedback
       </Button>
-
-      <Modal
-        open={modalFeedback}
-        onClose={() => setModalFeedback(false)}
-        aria-labelledby="modal-feedback-usuario"
-      >
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-          }}
-        >
-          <FeedbackUsuario />
-        </Box>
-      </Modal>
     </Router>
   );
 }
